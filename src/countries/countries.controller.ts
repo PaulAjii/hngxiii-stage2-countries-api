fabd5ebce29831b986d2af9570a0eb80815bdded
PaulAjii <paulajii134@gmail.com>
2025-10-28 05:09:25 +0100

    feat: created get all countries route

--- src/countries/countries.controller.ts
+++ src/countries/countries.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Query } from '@nestjs/common';
 import { CountriesService } from './countries.service';
+import { QueryDto } from './dto/query.dto';
 
 @Controller('countries')
 export class CountriesController {
   constructor(private countriesService: CountriesService) {}
 
   @Get()
-  getAllCountries() {
-    return this.countriesService.getAllCountries();
+  getAllCountries(@Query() filterQuery: QueryDto) {
+    return this.countriesService.filterCountry(filterQuery);
   }
 
   @Post('refresh')
