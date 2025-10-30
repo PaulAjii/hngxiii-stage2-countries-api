8755c87db2c3ee3ec32e8097618327ed04738a74
PaulAjii <paulajii134@gmail.com>
2025-10-30 15:22:55 +0100

    feat: add initial schema migration for country table

---
+++ src/migrations/1761833878455-InitialSchema.ts
@@ -0,0 +1,25 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class InitialSchema1761833878455 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      CREATE TABLE \`country\` (
+        \`id\` int NOT NULL AUTO_INCREMENT,
+        \`name\` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL UNIQUE,
+        \`capital\` varchar(255) NULL,
+        \`region\` varchar(255) NULL,
+        \`population\` int NOT NULL,
+        \`currency_code\` varchar(255) NOT NULL,
+        \`exchange_rate\` double NULL,
+        \`estimated_gdp\` double NULL,
+        \`flag_url\` varchar(255) NULL,
+        \`last_refreshed_at\` datetime NOT NULL,
+        PRIMARY KEY (\`id\`)
+      ) ENGINE=InnoDB;
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE \`country\``);
+  }
+}
